42a91b42c37eadd8113d96984686676c6074211c
Ruan Pretorius <pretoriusr.7@gmail.com>
2022-08-31 21:48:26 +0200

    added logging info

--- azure_mlops/src/models/dummy_model.py
+++ azure_mlops/src/models/dummy_model.py
@@ -1,6 +1,10 @@
 from __future__ import annotations
+
+import logging
 import random
 import pandas as pd
+
+logging.basicConfig(level=logging.INFO)
 
 
 class DummyClassifier:
@@ -9,13 +13,16 @@ class DummyClassifier:
     """
 
     def __init__(self) -> None:
+        logging.info("Initialising model...")
         self.params = {}
 
     def fit(self, X_train: pd.DataFrame, y_train: pd.Series) -> DummyClassifier:
         """Mimics training model on training data set (doesn't actually use data)."""
+        logging.info("Mimicking fitting model...")
         self.params = {"alpha": 0.1, "beta": 0.2, "gamma": 0.3, "delta": 0.4}
         return self
 
     def predict(self, X_test: pd.DataFrame) -> DummyClassifier:
         """Mimics predicting binary class for test data (doesn't actually use data)."""
+        logging.info("Mimicking creating predictions from model...")
         return random.choice([0, 1])
