c56290860235da3cac2280d8085f6e80eddbfb49
Ruan Pretorius <pretoriusr.7@gmail.com>
2022-08-31 21:48:40 +0200

    formated with black

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='azure_mlops',
-    packages=find_packages(where='.'),
-    version='0.0.1',
-    description='MLOps example implementations of CI/CD pipelines in Azure inspired by MLOps learning path from Microsoft.',
-    author='Ruan Pretorius',
-    license='',
+    name="azure_mlops",
+    packages=find_packages(where="."),
+    version="0.0.1",
+    description="MLOps example implementations of CI/CD pipelines in Azure inspired by MLOps learning path from Microsoft.",
+    author="Ruan Pretorius",
+    license="",
 )
